77e255a77ab7e009481dcc00a38b13588ace0842
ReyZ4YouXGod <raihanananda767@gmail.com>
2026-06-09 15:41:00 +0700

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,61 +1,24 @@
-const drop = document.getElementById("drop");
-const fileInput = document.getElementById("file");
-const bar = document.getElementById("bar");
-const result = document.getElementById("result");
-
-let selectedFile = null;
-
-// click to select
-drop.onclick = () => fileInput.click();
-
-fileInput.onchange = () => {
-    selectedFile = fileInput.files[0];
-    result.innerText = selectedFile.name;
-};
-
-// drag & drop
-drop.addEventListener("dragover", e => {
-    e.preventDefault();
-    drop.classList.add("dragover");
-});
-
-drop.addEventListener("dragleave", () => {
-    drop.classList.remove("dragover");
-});
-
-drop.addEventListener("drop", e => {
-    e.preventDefault();
-    drop.classList.remove("dragover");
-
-    selectedFile = e.dataTransfer.files[0];
-    result.innerText = selectedFile.name;
-});
-
-// upload
 async function uploadFile() {
-    if(!selectedFile) return alert("File belum dipilih");
+    let file = document.getElementById("file").files[0];
+    if(!file) return alert("Pilih file dulu");
+
+    let result = document.getElementById("result");
+    result.innerText = "Uploading...";
 
     let form = new FormData();
-    form.append("file", selectedFile);
-
-    result.innerText = "Uploading...";
-    bar.style.width = "20%";
+    form.append("file", file);
 
     try {
-        let res = await fetch("upload.php", {
+        let res = await fetch("/api/upload", {
             method: "POST",
             body: form
         });
 
-        bar.style.width = "70%";
-
         let data = await res.json();
-
-        bar.style.width = "100%";
 
         if(data.link){
             result.innerHTML = `
-                <p>Success!</p>
+                <p>Success:</p>
                 <a href="${data.link}" target="_blank">${data.link}</a>
             `;
 
@@ -66,6 +29,5 @@ async function uploadFile() {
 
     } catch(err){
         result.innerText = "Server error";
-        bar.style.width = "0%";
     }
 }
